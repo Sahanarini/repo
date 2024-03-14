c935eac0a56268259b5882f7708363006ced0f6c
srienathv2205 <srienathv2205@gmail.com>
2024-03-14 11:49:29 +0530

    Srienath modified Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,17 @@
 class Sample
 {
+  public void Div(int a,int b){
+	int result = a/b;
+	System.out.println(result);
+  }
+  public void Mul(int a,int b){
+	  int result = a*b;
+	  System.out.println(result);
+  }
   public static void main(String args[])
   {
-    System.out.println("github"):
+    int a=0,b=0;
+    System.out.println("github");
       
   }
 }
