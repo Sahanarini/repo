4aafe9d329b8c5b4182657c43dc94c1f025acec4
Shalz <shalinisekar@gmail.com>
2024-03-14 11:48:43 +0530

    File edited by Shalini

--- Sample.java
+++ Sample.java
@@ -1,6 +1,15 @@
 class Sample
 {
-  public static void main(String args[])
+public void add[int a,int b]{
+	int c = a+b;
+	System.out.println("after addition:" +c);
+
+}
+public void sub[int a,int b]{
+	int c = a-b;
+	System.out.println("after subtraction:"+c);
+}
+public static void main(String args[])
   {
     System.out.println("github"):
       
