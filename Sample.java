d837ab7ce15717eab96b7686d27654de71b7ebd4
shakeel.abdul <shakeelahmedcse@gmail.com>
2024-03-14 11:48:39 +0530

    file updated by shakeel

--- Sample.java
+++ Sample.java
@@ -1,6 +1,14 @@
 class Sample
 {
-  public static void main(String args[])
+ public void sub(int a, int b){
+	 int c = a-b;
+	 System.out.println("after subraction :" +c);
+ }
+ public void add(int a, int b){
+	 int c = a+b;
+	 System.out.println("after addition :" +c);
+ }	 
+ public static void main(String args[])
   {
     System.out.println("github"):
       
