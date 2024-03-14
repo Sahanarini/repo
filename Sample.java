a9c0f1be840d215062138bc95a71a8876a0d21f6
SUBASH <ssubash56781@gmail.com>
2024-03-14 11:45:07 +0530

    SUBASH

--- Sample.java
+++ Sample.java
@@ -1,5 +1,10 @@
 class Sample
 {
+  public void Mul(int a,int b)
+  {
+        int c = a*b;
+	System.out.println("Multiplication : "+c);
+  }
   public static void main(String args[])
   {
     System.out.println("github"):
