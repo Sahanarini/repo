df7a746d822e13649c0b195f04aed0aa06a5f5ec
SUBASH <ssubash56781@gmail.com>
2024-03-14 11:48:51 +0530

    Edited by SUBASH

--- Sample.java
+++ Sample.java
@@ -5,6 +5,11 @@ class Sample
         int c = a*b;
 	System.out.println("Multiplication : "+c);
   }
+  public void Sub(int a,int b)
+  {
+        int c = a-b;
+        System.out.println("Subtraction : "+c);
+  }
   public static void main(String args[])
   {
     System.out.println("github"):
